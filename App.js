0549f2afb564c41ed7ab6d3f266713ad2705bbee
unknown <dnringe@gmail.com>
2025-10-25 17:50:11 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello React World !!"
+);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
